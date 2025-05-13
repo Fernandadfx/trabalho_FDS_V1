4898d7ed7fedecfac8743b1911f27dde330b4adc
Fernandadfx <fernandadfxavier@gmail.com>
2025-05-13 19:46:15 -0300

    correções

---
+++ acoes.js
@@ -0,0 +1,4 @@
+            function mostrarmensagem(){
+                document.getElementById("mensagem").innerText = "Seja bem-vindo(a) à minha página! A persistência leva ao sucesso. 🚀";
+            }
+            
